716c128958a192cb5f36eace531a3271b48454df
mukunddp <mukundparve98@gmail.com>
2023-03-26 10:56:08 +0530

    Initial Commit

---
+++ 2-Inputs.py
@@ -0,0 +1,41 @@
+# name = input()
+# print(name)
+# print(id(name))
+#
+# b = 'Mukunda'
+# print(b)
+
+
+# name = input()
+# print(name)
+# print(type(name))       # type of that variable
+#
+# age = 25
+# print(age)
+# print(type(age))
+
+
+# age = int(input())
+# print(age)
+# print(type(age))
+
+
+# age = float(input())
+# print(age)
+# print(type(age))
+
+# age = bool(input())
+# print(age)
+# print(type(age))
+
+# name = "I'm \"Mukund Parve\""
+# print(name)
+
+
+# Personal Details as Input :
+# (name, age)
+
+# Give Introduction with personal details which are stored using Input
+name = 'Mukund'
+# f string
+print(f'I am {name}' + name)
---
+++ firstfile.py
@@ -0,0 +1,22 @@
+var = 'Mukund'
+print(var)
+print(id(var))
+
+name = 'Mukund'     # this is name
+print(name)
+print(id(name))     # this is checking memory location of DATA
+print(var)
+
+# S name S memory (3) or D name D memory(1)  NOTA(3)
+"""
+a = 110
+name = 'Mukund Parve'
+age = 25
+clg = 'PCCOE, Pune'
+print(name)
+NAME = 'Mukund Dattatraya Parve '
+print(NAME)
+print(name)
+"""
+
+

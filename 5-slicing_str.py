46495a137f9de0890eb28c35dc41f03e3d2fe51c
mukunddp <mukundparve98@gmail.com>
2023-04-09 16:11:01 +0530

    third week 1st lecture

--- 5-slicing_str.py
+++ 5-slicing_str.py
@@ -1,6 +1,16 @@
-word = 'There is no substitute for hard work'
-# listt = [10, 1, 15, 46464]
+# word = 'There is no substitute for hard work'
+listt = [10, 1, 15, 46464]
 
 
 # a_word = word[:15:]
 # print(a_word)
+
+
+# email = input('Enter your Mail address :')  # mukund.parve@techamplifiers.com
+# user_name = email[:email.index('@')]        #
+# raw_domain = email[email.index('@') + 1:]   #
+# Company_name = raw_domain[:raw_domain.index('.')]  # [start:end:step]
+# print(user_name + '\n' + Company_name)
+
+# email = 'mukund.parve@techamplifiers.com'
+# print(email[::-1])      # reverse
---
+++ 6-data_types.py
@@ -0,0 +1,75 @@
+# name = "Mukund Parve 1321@3424@##?"
+# print(type(name))
+
+# age = 15.5 + 45j
+# print(type(age))
+import array
+
+# L = [1, 5, 0, -1]               # Mutable , Slow
+# print(L)
+# print(type(L))
+#
+# T = 1, 5, 'Mukund', -1          # immutable , Fast
+# print(T)
+# print(type(T))
+# # T = (1, 5, 'Mukund', -1)
+# # print(T)
+# # print(type(T))
+#
+# L.append(15)
+# print(L)
+# T.append(15)
+# T.index(1)
+# name =  []
+# print(range(1, 15))     #
+# print(type(range(1, 15)))
+# a = list(range(1, 15))
+#
+# print(a)
+
+
+# range(start, stop-1, step)
+# a = range(0, 14, 4)  # start , stop = 15
+# print(a)        # 0, 4, 8, 12
+# print(list(a))  # start,  start +step, start + 2*step,
+
+
+# MAPPING Type : Dict :     # Mutable
+# d = {
+#     'Python': 'Programming Language',
+#     'age': 15,
+#     '15': 15
+# }
+# print(d)
+# print(d['age'])
+#
+# D = dict(Python='Programming Language')
+# print(D['Python'])
+
+
+# boolean :
+# a = True
+# print(type(a))
+# b = False
+# print(type(b))
+
+# set :     # Mutable / Unordered / unique elements
+# s = [10, 15, 5, 'Mukund', 4, 10]
+# # print(s.add(16))
+# # print(s)
+# print(s)
+# a = set(s)
+# print(a)
+
+# a = None
+# print(type(a))
+
+s = {1, 2, 'abc', 5j}       # Mutable
+print(s)
+s.add('pqr')   # None
+s1 = s
+
+
+
+
+

12a906d0bac0199aa6ce2c8c4656ae0fc0ef4958
mukunddp <mukundparve98@gmail.com>
2023-05-14 16:11:36 +0530

    13th May OOPs Completed

--- 19-Encapsulation.py
+++ 19-Encapsulation.py
@@ -14,17 +14,47 @@ class MyClass:
         self.__pin = 4564654  # private
 
     def method(self):
-        return self.name , self._debit_card, self.__pin
+        return self.name, self._debit_card, self.__pin
+
 
 class MyClass2(MyClass):
     def method2(self):
-        return self.name , self._debit_card, self.__pin
+        return self.name, self._debit_card, self.__pin
 
-obj = MyClass()
-# print(obj.name)
-# print(obj._debit_card)
-# print(obj.__pin)
 
-print(obj.method())
-obj2 = MyClass2()
-obj2.method2()
+# obj = MyClass()
+# # print(obj.name)
+# # print(obj._debit_card)
+# # print(obj.__pin)
+#
+# print(obj.method())
+# obj2 = MyClass2()
+# obj2.method2()
+
+
+# class test:
+#     def __init__(self, a="Hello World"):
+#         self.a = a
+#
+#     def display(self):
+#         print(self.a)
+#
+#
+# obj = test()
+# # obj.init()
+# obj.display()
+
+class fruits:
+
+    def __init__(self, price):
+        self.price = price
+
+
+obj = fruits(50)
+
+obj.quantity = 10
+
+obj.bags = 2
+
+print(obj.quantity + len(obj.__dict__))
+# print(obj.__dict__)
---
+++ 20-Exception_handling.py
@@ -0,0 +1,61 @@
+# Irregularity
+
+
+# a = 10
+# b = 20
+# # b = 'Mukund'
+# try:
+#     c = a + b
+# except:
+#     b = 0
+#     c = a + b
+#     print('This is exception code', c)
+# else:
+#     print('This is c from try :', c)
+# finally:
+#     print(c % 4)
+
+
+# a = 15
+# ZeroDivisionError
+# print(a / 0)
+
+# Value Error
+# ModuleNotFoundError
+# TypeError
+
+# try:
+#     a = int(input('Enter Positive number: '))
+#
+#     if a < 0:
+#         raise ValueError('This is not a positive Number')
+#     elif a == 1:
+#         raise ZeroDivisionError('Zerorooo')
+# except ValueError as v:
+#     print(v)
+# except ZeroDivisionError as z:
+#     print(z)
+# # except NameError as n:
+# #     print(n)
+# finally:
+#     print('code is running')
+
+
+# Python program to illustrate Function Annotations
+def fib(n: 'int', output: 'list' = []) -> 'list':
+    if n == 0:
+        return output
+    else:
+        if len(output) < 2:
+            output.append(1)
+            fib(n - 1, output)
+        else:
+            last = output[-1]
+            second_last = output[-2]
+            output.append(last + second_last)
+            fib(n - 1, output)
+        return output
+
+
+print(fib.__annotations__)
+print(fib(2, [15, 6, 25]))
